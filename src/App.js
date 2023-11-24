a3892bb358d2528bea14883a0a73706bfe1f3b83
niteshkarhe <nitesh.karhe@gmail.com>
2023-11-24 16:34:50 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import AudioRecorder from './page/component/AudioRecorder';
+
+function App() {
+  return (
+    <div className="App">
+      <AudioRecorder />
+    </div>
+  );
+}
+
+export default App;
